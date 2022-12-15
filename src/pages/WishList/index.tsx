45a05d6f8226cbf7bd45983598528eef8ec25b5f
LuizMeraki <luizollvrsantos@gmail.com>
2022-12-15 11:33:20 -0300

    Pages: the page name has been updated

---
+++ src/pages/WishList/index.tsx
@@ -0,0 +1,52 @@
+import { PageTitle } from "../../components/PageTitle";
+import { MiniProductCard } from '../../components/MiniProductCard/index';
+import { useFetchFavoriteProducts } from "../../hooks/useFetchFavoriteProducts";
+import { Loading } from "../../components/Loding";
+import { ErrorMessage } from "../../components/ErrorMessage";
+import styles from "./stlye.module.css";
+
+
+export const WishList = () => {
+
+  const { favoriteProducts, loading, error } = useFetchFavoriteProducts("1");
+
+  if (loading) {
+    return (
+      <main className="container-padding">
+        <Loading />
+      </main>
+    )
+  }
+
+
+  return (
+    <main className="container-padding">
+      <div className="max-width">
+        <PageTitle title="WishList" />
+        <div className={styles.productsContainer}>
+          {error &&
+            <ErrorMessage
+              className="text-center"
+              message={error}
+            />
+          }
+          {favoriteProducts?.data.length == 0 &&
+            <ErrorMessage
+              className="text-center"
+              message="You don't have any favorite products yet"
+            />
+          }
+          {favoriteProducts?.data.map((product) => (
+            <MiniProductCard
+              key={product.productId}
+              productPrice={product.productPrice}
+              productImageUrl={product.productImageUrl}
+              productName={product.productName}
+              productId={product.productId}
+            />
+          ))}
+        </div>
+      </div>
+    </main>
+  );
+}
