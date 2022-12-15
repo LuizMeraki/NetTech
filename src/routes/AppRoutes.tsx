1b1259dba3e10ac49f3162e03f349554d1f3058e
LuizMeraki <luizollvrsantos@gmail.com>
2022-12-15 11:30:03 -0300

    Pages: updated the page name import

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -10,7 +10,7 @@ import { Login } from '../pages/Login';
 import { AddProduct } from '../pages/AddProduct';
 import { ProductDetails } from "../pages/ProductDetails";
 import { Search } from "../pages/Search";
-import { FavoriteProducts } from "../pages/FavoriteProducts";
+import { WishList } from "../pages/WishList";
 import { CartPage } from '../pages/CartPage';
 import { PrivateRoute } from "../components/PrivateRoute";
 
@@ -30,7 +30,7 @@ export const AppRoutes = () => {
           <Route path="/add-product" element={<AddProduct />} />
         </Route>
         <Route path="/favorites" element={<PrivateRoute redirectTo="/login" />}>
-          <Route path="/favorites" element={<FavoriteProducts />} />
+          <Route path="/favorites" element={<WishList />} />
         </Route>
         <Route path="/cart" element={<PrivateRoute redirectTo="/login" />}>
           <Route path="/cart" element={<CartPage />} />
