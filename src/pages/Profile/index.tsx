cc8074a359a48debe77f9e25534b25ba3b13221b
LuizMeraki <luizollvrsantos@gmail.com>
2022-12-16 18:47:09 -0300

    Pages: a new page has been created

---
+++ src/pages/Profile/index.tsx
@@ -0,0 +1,14 @@
+import { PageTitle } from "../../components/PageTitle";
+import styles from "./style.module.css";
+
+
+export const Profile = () => {
+
+  return (
+    <main className="container-padding">
+      <div className="max-width">
+        <PageTitle title="Profile"/>
+      </div>
+    </main>
+  );
+}
