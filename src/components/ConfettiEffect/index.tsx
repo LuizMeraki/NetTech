c86e50b426029dcfe3aab00cc86ad597a817b64b
LuizMeraki <luizollvrsantos@gmail.com>
2022-12-16 18:45:02 -0300

    Component: a new component has been added

---
+++ src/components/ConfettiEffect/index.tsx
@@ -0,0 +1,49 @@
+import styles from "./style.module.css";
+
+
+export const ConfettiEffect = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.confetti}></div>
+      <div className={styles.confetti}></div>
+      <div className={styles.confetti}></div>
+      <div className={styles.confetti}></div>
+      <div className={styles.confetti}></div>
+      <div className={styles.confetti}></div>
+      <div className={styles.confetti}></div>
+      <div className={styles.confetti}></div>
+      <div className={styles.confetti}></div>
+      <div className={styles.confetti}></div>
+      <div className={styles.confetti}></div>
+      <div className={styles.confetti}></div>
+      <div className={styles.confetti}></div>
+      <div className={styles.confetti}></div>
+      <div className={styles.confetti}></div>
+      <div className={styles.confetti}></div>
+      <div className={styles.confetti}></div>
+      <div className={styles.confetti}></div>
+      <div className={styles.confetti}></div>
+      <div className={styles.confetti}></div>
+      <div className={styles.confetti}></div>
+      <div className={styles.confetti}></div>
+      <div className={styles.confetti}></div>
+      <div className={styles.confetti}></div>
+      <div className={styles.confetti}></div>
+      <div className={styles.confetti}></div>
+      <div className={styles.confetti}></div>
+      <div className={styles.confetti}></div>
+      <div className={styles.confetti}></div>
+      <div className={styles.confetti}></div>
+      <div className={styles.confetti}></div>
+      <div className={styles.confetti}></div>
+      <div className={styles.confetti}></div>
+      <div className={styles.confetti}></div>
+      <div className={styles.confetti}></div>
+      <div className={styles.confetti}></div>
+      <div className={styles.confetti}></div>
+      <div className={styles.confetti}></div>
+      <div className={styles.confetti}></div>
+      <div className={styles.confetti}></div>
+    </div>
+  );
+}
