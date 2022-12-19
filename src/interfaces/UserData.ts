02ba4b5ffd3798f2cd6a7cef666d5f792307c841
LuizMeraki <luizollvrsantos@gmail.com>
2022-12-19 15:53:45 -0300

    Interfaces: new type has been added

--- src/interfaces/UserData.ts
+++ src/interfaces/UserData.ts
@@ -1,7 +1,13 @@
 import { productDataType } from './Products';
 
 
+type authoritiesType = {
+  authority: string;
+}
+
 export type UserDataType = {
+  authorities: authoritiesType[];
+  username: string;
   favoriteProducts: productDataType[];
 }
 
